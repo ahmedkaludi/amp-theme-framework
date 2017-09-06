8827faa12ad7d8f1dc8d8ad7d61243bd8f2d5fb2
pegazee <abdulazeem.8179@gmail.com>
2017-09-06 18:59:47 +0530

    hook is added for call-now button

--- components/call-now/call-now.php
+++ components/call-now/call-now.php
@@ -1,5 +1,5 @@
 <?php // Call Now Button Feature added
-add_action('ampforwp_call_button','ampforwp_call_button_html_output');
+add_action('amp_call_button','amp_call_button_html_output');
 function amp_call_button_html_output(){
 	global $redux_builder_amp;
 	if ( $redux_builder_amp['ampforwp-callnow-button'] ) { ?>
