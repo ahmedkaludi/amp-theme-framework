62b2f3cd6a6981f3082598dc58058620b58e0bd8
pegazee <abdulazeem.8179@gmail.com>
2017-09-07 19:22:54 +0530

    proper code added for "Link to non-amp version" option

--- template/footer.php
+++ template/footer.php
@@ -4,7 +4,7 @@
           global $allowed_html;
           global $redux_builder_amp;
           echo wp_kses($redux_builder_amp['amp-translator-footer-text'],$allowed_html) ;
-          if($redux_builder_amp['amp-footer-link-non-amp-page']=='1') { ?> | <?php ampforwp_view_nonamp(); } 
+          if($redux_builder_amp['amp-footer-link-non-amp-page']=='1') { ampforwp_view_nonamp(); } 
           ?>
 </footer>
 <?php do_action('ampforwp_global_after_footer');
