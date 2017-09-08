276414240eb26425cc0a1bce2e88fe8bdcbd3a90
Mohammed Razzaq <marqas36@gmail.com>
2017-09-08 21:43:29 +0530

    Remove hooks

--- search.php
+++ search.php
@@ -1,10 +1,7 @@
 <?php amp_header() ?>
-	<?php do_action('ampforwp_post_before_loop') ?>
 
     <?php amp_archive_title(); ?>
     <?php amp_loop_template(); ?>
     <?php amp_pagination(); ?>
-
-	<?php do_action('ampforwp_post_after_loop') ?>
-
+    
 <?php amp_footer(); ?>
